5ad8c79ff37569aa2d2f77aeb24b8e059254202f
selfridges-susanna <susiparo1@gmail.com>
2024-06-24 20:21:16 +0100

    Fetch variables setup

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-function hello () {
-    return "hello"
-}
+const WEATHER_API_KEY = "66d6dac659158714ecc7ae920d07ddb9"
+const lat = 33.44
+const lon = -94.04
+const part = "daily"
+
+const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${WEATHER_API_KEY}`
+
+console.log(url)
